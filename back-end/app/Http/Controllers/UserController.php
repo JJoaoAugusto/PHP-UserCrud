9b4374a0b1fe4931567a6812eb3753eacf5b2132
João Augusto <jaugustovpires@gmail.com>
2025-07-21 23:00:10 -0300

    feat: UserController show route

--- back-end/app/Http/Controllers/UserController.php
+++ back-end/app/Http/Controllers/UserController.php
@@ -12,8 +12,8 @@ class UserController extends Controller
      */
     public function index()
     {
-        return User::all();
-
+        $users = User::all();
+        return response()->json($users, 200);
     }
 
     /**
@@ -29,7 +29,8 @@ class UserController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $user = User::findOrFail($id);
+        return response()->json($user, 200);
     }
 
     /**
